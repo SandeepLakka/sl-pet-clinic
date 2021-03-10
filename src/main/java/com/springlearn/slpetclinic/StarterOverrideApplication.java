42deda447d9e47f8a535e7ff7bce9902126d0f94
SandeepLakka <79764577+sandeeplakka@users.noreply.github.com>
2021-03-10 22:02:16 +0530

    Including Spring starter template code

---
+++ src/main/java/com/springlearn/slpetclinic/StarterOverrideApplication.java
@@ -0,0 +1,13 @@
+package com.springlearn.slpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterOverrideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterOverrideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springlearn/slpetclinic/StarterOverrideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlearn.slpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterOverrideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
