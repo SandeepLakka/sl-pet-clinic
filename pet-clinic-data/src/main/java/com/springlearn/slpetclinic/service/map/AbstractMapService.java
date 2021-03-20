36ab17db1a0d334def9185d92479bea9fe9dfa3d
SandeepLakka <79764577+sandeeplakka@users.noreply.github.com>
2021-03-20 14:12:20 +0530

    Restricting null objects into Map Impl Services

--- pet-clinic-data/src/main/java/com/springlearn/slpetclinic/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/springlearn/slpetclinic/service/map/AbstractMapService.java
@@ -23,6 +23,8 @@ public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> 
                 object.setId(getNextId());
             }
             map.put(object.getId(), object);
+        }else{
+            throw new RuntimeException("Object cannot be Empty");
         }
 
         return object;
