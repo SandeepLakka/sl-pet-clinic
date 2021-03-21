3eb17bdaa68f4e17fe1b7c983ba079d4aa2c384b
SandeepLakka <79764577+sandeeplakka@users.noreply.github.com>
2021-03-21 10:58:22 +0530

    Added Speciality and Associated to Vet Closes #15

---
+++ pet-clinic-data/src/main/java/com/springlearn/slpetclinic/model/Speciality.java
@@ -0,0 +1,8 @@
+package com.springlearn.slpetclinic.model;
+
+import lombok.Data;
+
+@Data
+public class Speciality extends BaseEntity {
+    private String description;
+}
--- pet-clinic-data/src/main/java/com/springlearn/slpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/springlearn/slpetclinic/model/Vet.java
@@ -1,8 +1,14 @@
 package com.springlearn.slpetclinic.model;
 
+import lombok.Data;
 import lombok.ToString;
 
+import java.util.Set;
+
+@Data
 @ToString(callSuper = true)
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
 
 }
