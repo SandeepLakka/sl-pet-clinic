4690585d6470bec7b1776fbc8bac25ffd84930a8
SandeepLakka <79764577+sandeeplakka@users.noreply.github.com>
2021-03-21 20:38:50 +0530

    Fixed broken menu links Closes #34

--- pet-clinic-web/src/main/java/com/springlearn/slpetclinic/controller/IndexController.java
+++ pet-clinic-web/src/main/java/com/springlearn/slpetclinic/controller/IndexController.java
@@ -1,12 +1,12 @@
 package com.springlearn.slpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping({"","index","index.html"})
+    @GetMapping({"","/", "/index","/index.html"})
     public String index(){
         return "index";
     }
--- pet-clinic-web/src/main/java/com/springlearn/slpetclinic/controller/OwnersController.java
+++ pet-clinic-web/src/main/java/com/springlearn/slpetclinic/controller/OwnersController.java
@@ -1,11 +1,14 @@
 package com.springlearn.slpetclinic.controller;
 
 import com.springlearn.slpetclinic.service.OwnerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
+@RequestMapping("/owners")
 public class OwnersController {
 
     private final OwnerService ownerService;
@@ -14,9 +17,15 @@ public class OwnersController {
         this.ownerService = ownerService;
     }
 
-    @GetMapping({"/owners", "/owners/index", "/owners/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners(Model model){
         model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
+    }
+
+    //TODO: Implement owners/find endpoint. Currently it is NYI
+    @RequestMapping("/find")
+    public String findOwners(){
+        return "notimplemented";
     }
 }
